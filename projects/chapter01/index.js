93b65c2a680d344412a754c0f98ff12a0094ad56
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-09-08 10:10:23 -0400

    finishing chapter 1

---
+++ projects/chapter01/index.js
@@ -0,0 +1,11 @@
+// print name
+console.log("Rodney");
+
+// print age
+console.log(23);
+
+// print add, subtract, multiply, and divide 6 by 3
+console.log(6+3);
+console.log(6-3);
+console.log(6*3);
+console.log(6/3);
